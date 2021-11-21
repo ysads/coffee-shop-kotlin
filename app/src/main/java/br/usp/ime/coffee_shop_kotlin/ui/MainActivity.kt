2e379848850c3f6a79dda3481eac0b4543114eef
Ygor <ysads2010@gmail.com>
2021-11-21 16:28:01 +0100

    Beautify MainActivity

--- app/src/main/java/br/usp/ime/coffee_shop_kotlin/ui/MainActivity.kt
+++ app/src/main/java/br/usp/ime/coffee_shop_kotlin/ui/MainActivity.kt
@@ -3,46 +3,44 @@ package br.usp.ime.coffee_shop_kotlin.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.ImageView
-import android.widget.TextView
 import br.usp.ime.coffee_shop_kotlin.adapters.ProductListAdapter
 import br.usp.ime.coffee_shop_kotlin.data.products.ProductsService
 import br.usp.ime.coffee_shop_kotlin.databinding.ActivityMainBinding
+import br.usp.ime.coffee_shop_kotlin.domain.Product
 import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var products: ArrayList<Product>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val firstName: TextView = binding.firstName
-        firstName.text = intent.getStringExtra("firstName")
+        loadUser()
+        loadProductsIntoList()
+    }
 
-        val lastName: TextView = binding.lastName
-        lastName.text = intent.getStringExtra("lastName")
-
-        val pic: ImageView = binding.pic
+    private fun loadUser() {
         val picURL = intent.getStringExtra("picURL")
+
+        binding.name.text =
+            intent.getStringExtra("firstName") + " " + intent.getStringExtra("lastName")
         Picasso.get()
             .load(picURL)
-            .into(pic)
+            .into(binding.pic)
+    }
 
-        Log.i("First Name", firstName.toString())
-        Log.i("Last Name", lastName.toString())
-        Log.i("Pic URL", picURL.toString())
-
-        val productsArrayList = ProductsService().getAll()
+    private fun loadProductsIntoList() {
+        products = ProductsService().getAll()
 
         binding.listview.isClickable = true
-        binding.listview.adapter = ProductListAdapter(this, productsArrayList)
+        binding.listview.adapter = ProductListAdapter(this, products)
         binding.listview.setOnItemClickListener { _, _, position, _ ->
             val intent = Intent(this, ProductDetailsActivity::class.java)
-            intent.putExtra("product", productsArrayList[position])
+            intent.putExtra("product", products[position])
             startActivity(intent)
         }
     }
